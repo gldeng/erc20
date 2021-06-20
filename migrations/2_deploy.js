67417072c398f8380642cd358ebfca3224173593
gldeng <realgldeng@gmail.com>
2021-06-20 08:30:41 +0800

    Init with simple token

---
+++ migrations/2_deploy.js
@@ -0,0 +1,8 @@
+// migrations/2_deploy.js
+const SimpleToken = artifacts.require('SimpleToken');
+
+module.exports = async function (deployer) {
+  const zero_18 = '000000000000000000'
+  await deployer.deploy(SimpleToken, 'USD Token', 'USD',    '10000' + zero_18);
+  await deployer.deploy(SimpleToken, 'Telsa Token', 'TSLA', '100' + zero_18);
+};
