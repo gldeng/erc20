4517e0b3ce21ce804b3b974218160684e33790d7
gldeng <realgldeng@gmail.com>
2021-06-20 13:38:48 +0800

    Add HTLC contract

--- migrations/2_deploy.js
+++ migrations/2_deploy.js
@@ -1,8 +1,10 @@
 // migrations/2_deploy.js
 const SimpleToken = artifacts.require('SimpleToken');
+const HashedTimelockERC20 = artifacts.require('HashedTimelockERC20');
 
 module.exports = async function (deployer) {
   const zero_18 = '000000000000000000'
   await deployer.deploy(SimpleToken, 'USD Token', 'USD',    '10000' + zero_18);
   await deployer.deploy(SimpleToken, 'Telsa Token', 'TSLA', '100' + zero_18);
+  await deployer.deploy(HashedTimelockERC20);
 };
